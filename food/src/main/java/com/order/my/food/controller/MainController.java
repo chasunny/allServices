de26b0c4821848bea434ae7554fad643151cc864
chasunny <49181767+chasunny@users.noreply.github.com>
2022-08-15 22:26:08 +0530

    Update MainController.java

--- food/src/main/java/com/order/my/food/controller/MainController.java
+++ food/src/main/java/com/order/my/food/controller/MainController.java
@@ -10,7 +10,7 @@ package com.order.my.food.controller;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.data.jpa.repository.Modifying;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.RestController;
 
