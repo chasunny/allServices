5f0424ce97b4933f0a2ee8b55f7a3d37dd8cee10
chasunny <49181767+chasunny@users.noreply.github.com>
2022-08-15 22:23:22 +0530

    Update MainController.java

--- food/src/main/java/com/order/my/food/controller/MainController.java
+++ food/src/main/java/com/order/my/food/controller/MainController.java
@@ -1,38 +1,38 @@
 package com.order.my.food.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import javax.persistence.EntityManager;
+// import javax.persistence.EntityManager;
 
-import javax.transaction.Transactional;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+// import javax.transaction.Transactional;
 
 
-import com.order.my.food.feignproxy.FoodProxy;
-import com.order.my.food.model.DeliveryBoy;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.jpa.repository.Modifying;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.order.my.food.model.Restaurant;
 
-import com.order.my.food.service.RestServices;
+// import com.order.my.food.feignproxy.FoodProxy;
+// import com.order.my.food.model.DeliveryBoy;
+
+// import com.order.my.food.model.Restaurant;
+
+// import com.order.my.food.service.RestServices;
 
 @RestController 
 public class MainController {
 	
-	@Autowired
-	FoodProxy prxy;
+// 	@Autowired
+// 	FoodProxy prxy;
 	
-	@Autowired
-	RestServices rs;
+// 	@Autowired
+// 	RestServices rs;
 	
-	@Autowired
-	EntityManager em;
+// 	@Autowired
+// 	EntityManager em;
 	
 	
 	
@@ -42,7 +42,7 @@ public class MainController {
 	
 	List<Restaurant> temp=new ArrayList<Restaurant>();
 	
-	private static final String TOPIC ="order_service";
+// 	private static final String TOPIC ="order_service";
 	
 	//kafka
 //	@GetMapping("/greeting/{message}")
