4c5fa2b984f43b88474e909e91549bbc8f7dbb32
chasunny <49181767+chasunny@users.noreply.github.com>
2022-08-16 22:58:08 +0530

    Update Order.java

--- food/src/main/java/com/order/my/food/model/Order.java
+++ food/src/main/java/com/order/my/food/model/Order.java
@@ -1,13 +1,13 @@
 package com.order.my.food.model;
 
-import java.sql.Time;
-import java.util.Date;
+// import java.sql.Time;
+// import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
 
 //@Entity
 //@Table(name ="orders")
