ee359fd309143054cb1661ed2bc9c0db7609842a
chasunny <49181767+chasunny@users.noreply.github.com>
2022-08-16 22:57:50 +0530

    Update Restaurant.java

--- food/src/main/java/com/order/my/food/model/Restaurant.java
+++ food/src/main/java/com/order/my/food/model/Restaurant.java
@@ -1,27 +1,27 @@
 package com.order.my.food.model;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.List;
+// import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
+// import javax.persistence.CascadeType;
+// import javax.persistence.CollectionTable;
+// import javax.persistence.Column;
+// import javax.persistence.ElementCollection;
+// import javax.persistence.Entity;
+// import javax.persistence.FetchType;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.JoinTable;
+// import javax.persistence.ManyToMany;
+// import javax.persistence.ManyToOne;
+// import javax.persistence.OneToMany;
+// import javax.persistence.Table;
+// import javax.validation.constraints.Size;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 
 //@Entity
 public class Restaurant {
